ec60fc9fb858e478d7c961dfa7dc1509fa0085e4
Kieran Hayes <kieran.hayes@dkd.de>
2015-08-11 15:23:18 +0200

    remove strip_tags

--- app/helpers/redsun_search_helper.rb
+++ app/helpers/redsun_search_helper.rb
@@ -4,14 +4,11 @@ module RedsunSearchHelper
     render :partial => partial, :locals => { :facet => name, :results => results, :attribute => attribute }
   end
 
-  def highlighter_for(field, hit, record, strip_tags = false)
+  def highlighter_for(field, hit, record)
     if hit.highlights(field).present?
-      sanitized_field = strip_tags == true ? strip_tags(hit.highlights(field)) : hit.highlights(field)
-      sanitized_field.collect { |segment| segment.format { |word| "<span class='redsun-highlight'>#{word}</span>" }}.join(" ").html_safe
+      hit.highlights(field).collect { |segment| segment.format { |word| "<span class='redsun-highlight'>#{word}</span>" }}.join(" ").html_safe
     else
-      if record.respond_to?(field.to_sym)
-        strip_tags == true ? strip_tags(record.send(field)) : record.send(field)
-      end
+      record.send(field) if record.respond_to?(field.to_sym)
     end
   end
 
